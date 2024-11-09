f22230b497dc8b0aa4741c989f6e3893caadf4be
LindaGlahy <lindaglahy@gmail.com>
2024-11-09 02:23:36 +0000

    modifications and creations

--- AED/Ficha de exercicios 4/Ex7.py
+++ AED/Ficha de exercicios 4/Ex7.py
@@ -1,13 +1,26 @@
 meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
+def pluviosidade(listPluvio):
+    '''
+    '''
+    listPluvio.sort(reverse=True)
+    for i in range(12):
+        print(listPluvio[i])
+
+def pluviosidade2(listPluvio):
+    '''
+    '''
+    listPluvio2 = listPluvio.copy()
+    listPluvio2.sort(reverse=True)
+    print("\n")
+    print("Meses \t Pluviosidade")
+    for i in range(12):
+        pos = listPluvio.index(listPluvio2)
+        print(meses[pos], "\t", listPluvio2[i])
+
 listPluvio = []
-leitura = 0
 for i in range(12):
     leitura = int(input("Indique a faturação do mês de {:s}: " .format(meses[i])))
-    novaLeitura = leitura
-    if novaLeitura > leitura:
-        listPluvio = (novaLeitura) + listPluvio
-    elif novaLeitura <= leitura:
-        listPluvio.append(novaLeitura)
+    listPluvio.append(leitura)
 
-print (listPluvio)
+pluviosidade2(listPluvio)
